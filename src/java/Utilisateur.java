3971afd92a7614515984806b2015412225540ca8
Majd-rebai <majdrebai2007@gmail.com>
2018-01-30 11:23:12 +0100

    v0.1

---
+++ src/java/Utilisateur.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String password;
+    
+    
+    
+}
